4c34cdb6498d44f86f88271a8e92d96ee4e8f2e4
Joaquin <joaquin050416@gmail.com>
2022-09-26 09:28:14 -0500

    Correción relaciones

--- cliente/src/ConfigDataBase/DataBase.orm.js
+++ cliente/src/ConfigDataBase/DataBase.orm.js
@@ -77,46 +77,46 @@ const comentario = comentariosModelo(sequelize, Sequelize)
 //RELACIONES
 //CLIENTE
 //Cliente-ubicacioncliente
-cliente.hasmany(ubicacion_cliente)// hasmany a donde va la relacion
-ubicacion_cliente.belongto(cliente)// belongto de donde viene
+cliente.hasMany(ubicacion_cliente)// hasmany a donde va la relacion
+ubicacion_cliente.belongsTo(cliente)// belongto de donde viene
 
 //cliente-detalleCliente
-cliente.hasmany(detalle_cliente)
-detalle_cliente.belongto(cliente)
+cliente.hasMany(detalle_cliente)
+detalle_cliente.belongsTo(cliente)
 
 //cliente-bicicleta
-cliente.hasmany(bicicleta)
-bicicleta.belongto(cliente)
+cliente.hasMany(bicicleta)
+bicicleta.belongsTo(cliente)
 
 //BICICLETA
 //bicicleta-perdidaRobada
-bicicleta.hasmany(perdidaRobada)
-perdidaRobada.belongto(bicicleta)
+bicicleta.hasMany(perdidaRobada)
+perdidaRobada.belongsTo(bicicleta)
 
 //bicicleta-rin
-bicicleta.hasmany(rin)
-rin.belongto(bicicleta)
+bicicleta.hasMany(rin)
+rin.belongsTo(bicicleta)
 
 //Grupos-detalleGrupo
-grupos.hasmany(detalleGrupos)
-detalleGrupos.belongto(grupos)
+grupos.hasMany(detalleGrupos)
+detalleGrupos.belongsTo(grupos)
 
 //detalleGrupo-tipoCiclismo
-detalleGrupos.hasmany(tiposCiclismo)
-tiposCiclismo.belongto(detalleGrupos)
+detalleGrupos.hasMany(tiposCiclismo)
+tiposCiclismo.belongsTo(detalleGrupos)
 
 //tipoCiclismo-detalleCliente
-tiposCiclismo.hasmany(detalle_cliente)
-detalle_cliente.belongto(tiposCiclismo)
+tiposCiclismo.hasMany(detalle_cliente)
+detalle_cliente.belongsTo(tiposCiclismo)
 
 //COMENTARIOS
 //bicleta-comentario
-bicicleta.hasmany(comentario)
-comentario.belongto(bicicleta)
+bicicleta.hasMany(comentario)
+comentario.belongsTo(bicicleta)
 
 //grupo-comentario
-grupos.hasmany(comentario)
-comentario.belongto(grupos)
+grupos.hasMany(comentario)
+comentario.belongsTo(grupos)
 
 module.exports = {
     cliente,
