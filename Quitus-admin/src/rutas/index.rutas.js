b3096200ce5dc73ba0df6c94e639f7c826c738af
KevinSaltos <KevnSaltos@hotmail.com>
2022-09-29 20:39:30 -0500

    .

--- Quitus-admin/src/rutas/index.rutas.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const rutas = express.Router()
-
-const { mostrar } = require('../controladores/indexControlador')
-
-rutas.get('/', mostrar)
-
-module.exports = rutas
--- Quitus-admin/src/rutas/usuario.rutas.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const rutas = express.Router()
-
-const { mostrar } = require('../controladores/loginControlador')
-
-rutas.get('/login', mostrar)
-
-module.exports = rutas
