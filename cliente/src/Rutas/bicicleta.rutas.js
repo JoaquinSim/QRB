9d0c01f04252d51fcb276a29ff7914cee017559f
Joaquin <joaquin050416@gmail.com>
2022-10-03 12:50:01 -0500

    Ultima modificacion apartado de grupo

--- cliente/src/Rutas/bicicleta.rutas.js
+++ cliente/src/Rutas/bicicleta.rutas.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { mostrarBicicleta } = require('../Controllers/bicicleta.controller');
+const { mostrarBicicleta, agregarBicicleta, editarBicicleta } = require('../Controllers/bicicleta.controller');
 
 const router = express.Router();
 
-router.get('/bicicleta/inicio', mostrarBicicleta)
+router.get('/bicicleta/agregar', agregarBicicleta)
+
+router.get('/bicicleta/listar', mostrarBicicleta)
+
+router.get('/bicicleta/editar', editarBicicleta)
+
 
 module.exports = router
